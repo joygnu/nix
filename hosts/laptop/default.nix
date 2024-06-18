cb3cc2aabcbab344578182b88c89bb1b86c35953
joygnu <mail@joygnu.org>
2024-06-18 17:49:46 +0200

    lol

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -2,7 +2,6 @@
 {
   networking.hostName = "laptop";
   networking.networkmanager.enable = true;
-
   hardware.nvidia.nvidiaSettings = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
