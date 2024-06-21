2f1e7eeb8bb611f261abc211fde429a144810634
joy <mail@joygnu.org>
2024-06-21 23:30:00 +0200

    retro

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -1,9 +1,18 @@
 { config, inputs, pkgs, ... }:
 
 {
-services.openssh = {
-  enable = true;
-  settings.PasswordAuthentication = false;
-  settings.KbdInteractiveAuthentication = false;
-};
+  services.openssh = {
+    enable = true;
+    # require public key authentication for better security
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = true;
+    #settings.PermitRootLogin = "yes";
+  };
+  
+ # users.users.joy.openssh.authorizedKeys.keys = [
+ # "ssh-rsa AAAAB3Nz....6OWM= user" # content of authorized_keys file
+  # note: ssh-copy-id will add user@your-machine after the public key
+  # but we can remove the "@your-machine" part
+#];
+ 
 }
