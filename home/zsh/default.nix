136d50bd3bffcb2c13ec8b962c0be7f67bff3805
joy <mail@joygnu.org>
2024-07-01 08:10:28 +0200

    zsh

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -9,10 +9,11 @@
 
     shellAliases = {
       v = "nvim";
-      sp = "sudo nixos-rebuild switch --flake ~/nix/#pc";
-      sl = "sudo nixos-rebuild switch --flake ~/nix/#laptop";
+      pu = "git push -u origin main && git push -u origin berg && git push -u hub";
       fr = "sudo systemctl reboot --firmware";
       ser = "ssh root@joygnu.org";
+      sp = "sudo nixos-rebuild switch --flake ~/nix/#pc";
+      sl = "sudo nixos-rebuild switch --flake ~/nix/#laptop";
       up = "sudo nix flake update ~/nix";
       del = "sudo nix-collect-garbage -d";
       gtk = "nvim ~/nix/home/gtk/default.nix";
