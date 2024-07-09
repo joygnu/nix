f6bf2e58d47eea1c503bb38733085daa1fdd4a4b
joy <mail@joygnu.org>
2024-07-09 22:27:56 +0200

    update

---
+++ modules/update/default.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+      randomizedDelaySec = "45min";
+    };
+
+}
