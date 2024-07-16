19776d50d93ea01854997a0f07bda27dc11db18b
joygnu <mail@joygnu.org>
2024-07-16 20:59:37 +0200

    helix

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -2,5 +2,14 @@
 {
   programs.helix = {
     enable = true;
+    defaultEditor = true;
+    settings = {
+     # lsp.display-messages = true;
+      keys.normal = {
+        space.space = "file_picker";
+       # Z.Z = ":wq";
+       # Z.Q = ":q";
+      };
+    };
   };
 }
