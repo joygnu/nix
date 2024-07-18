46ebb8075c656016d86c27b96368ffc0a03b2f6b
joy <mail@joygnu.org>
2024-07-18 14:48:36 +0200

    no more vim

--- flake.nix
+++ flake.nix
@@ -6,18 +6,17 @@
     ags.url = "github:Aylur/ags";
     stylix.url = "github:danth/stylix";
 
-  };
-  firefox-addons = {
-    url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-    inputs.nixpkgs.follows = "nixpkgs";
+  
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   
-
-  home-manager = {
-    url = "github:nix-community/home-manager";
-    inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
