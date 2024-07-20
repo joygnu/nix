2d034db223a806e7e1990d4497e01905c373e955
joy <mail@joygnu.org>
2024-07-20 14:33:01 +0200

    sensors

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,21 +1,17 @@
 { pkgs, lib, ... }: 
 {
 
-    programs.rofi = {
-      enable = true;
-      package = pkgs.rofi-wayland;
-      theme = lib.mkForce "~/nix/home/rofi/gruvbox-material.rasi";
-      extraConfig = {
-        modi = "drun";
-        icon-theme = "Oranchelo";
-        show-icons = true;
-        terminal = "alacritty";
-        drun-display-format = "{icon} {name}";
-        disable-history = false;
-        hide-scrollbar = true;
-        display-drun = "   Apps ";
-        display-network = " 󰤨  Network";
-        sidebar-mode = false;
-      };
-};
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+    theme = lib.mkForce "~/nix/home/rofi/gruvbox-material.rasi";
+    extraConfig = {
+      display-ssh=    "";
+      display-run=    "";
+      display-drun=   "";
+      display-window= "";
+      display-combi=  "";
+      show-icons=     true;
+    };
+  };
 }
