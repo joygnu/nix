27bbdb9f48a09e6660d647c95c1fb4697fc31bc9
joygnu <mail@joygnu.org>
2024-08-04 00:37:19 +0200

    shorter flake

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -6,12 +6,13 @@
     package = pkgs.rofi-wayland;
     theme = lib.mkForce "~/nix/home/rofi/gruvbox-material.rasi";
     extraConfig = {
-      display-ssh=    "";
-      display-run=    "";
-      display-drun=   "";
-      display-window= "";
-      display-combi=  "";
-      show-icons=     true;
+      display-ssh =    "";
+      display-run =    "";
+      display-drun =   "";
+      display-window = "";
+      display-combi =  "";
+      show-icons =    true;
+      terminal = "alacritty";
     };
   };
 }
