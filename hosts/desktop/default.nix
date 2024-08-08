432a551da4baeaa82df957660e32c0440bd11f14
joygnu <mail@joygnu.org>
2024-08-08 23:07:44 +0200

    soystemd boot

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -3,17 +3,10 @@
   networking.hostName = "desktop";
   networking.networkmanager.enable = true;
   services.hardware.openrgb.enable = true; 
-  
-  boot.loader = {
-   timeout = 0;
-    efi.canTouchEfiVariables = false;
-    grub = {
-      efiSupport = true;
-      efiInstallAsRemovable = true;
-      device = "nodev";
-    };
-  }; 
  
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   imports = [
     ../../modules
     ./hardware.nix
