6e340f6a45887a307118db68fed9781f8fd99740
joy <mail@joygnu.org>
2024-08-08 17:25:49 +0200

    unfree NOOOOOOOOOOOOOOOOOOOOOo

---
+++ hosts/desktop/unfree.nix
@@ -0,0 +1,12 @@
+{ pkgs, lib, ... }:
+{
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+  ];
+  programs.steam.enable = true;
+  environment.systemPackages = with pkgs; [
+    prismlauncher
+    mangohud
+    lutris
+  ];
+}
