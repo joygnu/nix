77d47ab3d0ae8f1acda2622f8fdca972803191b0
joy <mail@joygnu.org>
2024-08-09 18:27:47 +0200

    hypridle

--- home/hypridle/default.nix
+++ home/hypridle/default.nix
@@ -2,5 +2,19 @@
 {
   services.hypridle = {
     enable = true;
-  };
+      settings = {
+        general = {
+           after_sleep_cmd = "hyprctl dispatch dpms on";
+           ignore_dbus_inhibit = false;
+         };
+       
+         listener = [
+           {
+             timeout = 1200;
+             on-timeout = "hyprctl dispatch dpms off";
+             on-resume = "hyprctl dispatch dpms on";
+           }
+         ];
+       };
+    };
 }
