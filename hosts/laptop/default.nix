79fec0bee7faf64418173566357aa3be80ca122b
joy <mail@joygnu.org>
2024-08-13 17:07:26 +0200

    adding shit for normis lllll

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,11 +1,14 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   networking.hostName = "laptop";
   networking.networkmanager.enable = true;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  
+  environment.systemPackages = with pkgs; [
+    prismlauncher
+  ];
   imports = [
     ../../modules
     ./hardware.nix
