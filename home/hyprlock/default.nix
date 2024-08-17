0a5771ff80e7c527a4c82b680cf6cd923c691407
joy <mail@joygnu.org>
2024-08-17 14:47:05 +0200

    hyprlock

--- home/hyprlock/default.nix
+++ home/hyprlock/default.nix
@@ -2,7 +2,35 @@
 {
   programs.hyprlock = {
     enable = true;
-      # settings = {
-      # };
-    };
+      settings = {
+        general = {
+          grace = 300;
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+      
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+      
+        input-field = [
+          {
+            size = "200, 50";
+            position = "0, -80";
+            monitor = "";
+            dots_center = true;
+            fade_on_empty = false;
+            font_color = "rgb(209, 193, 158)";
+            inner_color = "rgb(40, 40, 40)";
+            outer_color = "rgb(69, 133, 136)";
+            outline_thickness = 5;
+            shadow_passes = 2;
+          }
+        ];
+      };      
+  };
 }
