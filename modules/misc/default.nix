c331bcd5f57b5727179a868a0793e3b6ad704cc1
joygnu <mail@joygnu.org>
2024-08-18 15:32:20 +0200

    doas

--- modules/misc/default.nix
+++ modules/misc/default.nix
@@ -11,7 +11,7 @@
   users.users.joy = {
     isNormalUser = true;
     description = "joy";
-    extraGroups = [ "wheel" "docker" ];
+    extraGroups = [ "wheel" "docker" "foo" ];
   };  
  
   virtualisation.docker.rootless.enable = true;
