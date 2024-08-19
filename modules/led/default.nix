21029bc9afcaaf853dd72dc94cef2ba08f517509
joy <mail@joygnu.org>
2024-08-19 21:20:58 +0200

    led

---
+++ modules/led/default.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  systemd.services.micmute = {
+    description = "Set microphone mute and normal mute LEDs to off";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "/bin/sh -c 'echo 0 | tee /sys/class/leds/platform::micmute/brightness > /dev/null; echo 0 | tee /sys/class/leds/platform::mute/brightness > /dev/null'";
+      Type = "oneshot";
+      User = "root";
+      Group = "root";
+    };
+  };
+}
