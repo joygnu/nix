7a883028dcc719fc4cea7054eba9311077422c6d
joy <mail@joygnu.org>
2024-08-21 22:35:01 +0200

    simpler rebuild.sh

--- home/zsh/rebuild.sh
+++ home/zsh/rebuild.sh
@@ -3,9 +3,7 @@
 hostname=$(hostname)
 
 if [[ $hostname == *"desktop"* ]]; then
-    sudo nixos-rebuild switch --flake ~/nix/#desktop &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
-    rm nixos-switch.log
+    sudo nixos-rebuild switch --flake ~/nix/#desktop 
 elif [[ $hostname == *"laptop"* ]]; then
-    sudo nixos-rebuild switch --flake ~/nix/#laptop &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
-    rm nixos-switch.log
+    sudo nixos-rebuild switch --flake ~/nix/#laptop 
 fi
