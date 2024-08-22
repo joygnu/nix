6c57a16a6ad90a1124b08f672d1fa8ad7d8cfa67
joy <mail@joygnu.org>
2024-08-22 07:57:54 +0200

    email sync services

--- home/neomutt/default.nix
+++ home/neomutt/default.nix
@@ -1,16 +1,4 @@
 {pkgs, ...}: {
-  # programs.neomutt = {
-  # enable = true;
-  # sidebar.enable = true;
-  # vimKeys = true;
-  # };
-  # programs.mbsync = {
-  # enable = true;
-  # };
-  # programs.msmtp = {
-  # enable = true;
-  # };
-
   home.packages = with pkgs; [
     mutt-wizard
     neomutt
