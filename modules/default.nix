f5314eb9cc19d323edefe0638bf5c62d076e46b0
joy <mail@joygnu.org>
2024-08-31 16:35:44 +0200

    everything works

--- modules/default.nix
+++ modules/default.nix
@@ -1,39 +1,11 @@
 {
   imports = [
-    # ./fonts
-    # ./sound
-    # ./vm
-    # ./packages
-    # ./misc
-    # ./gdm
-    # ./stylix
-    # ./blue
-    # ./services
-    # ./syncthing
-    # ./gpg
-    # ./appimage
-    # ./update
-    # ./polkit
-    # ./ssh
-    # ./dunst
-    # ./zsh
-    # ./misc
-    # ./rofi
-    # ./helix
-    # ./firefox
-    # ./alacritty
-    # ./hyprland
-    # ./hypridle
-    # ./newsboat
-    # ./git
-    # ./gtk
-    # ./ags
    ./apps  
-  # ./desktop  
-  ./hardware  
-  # ./services	
-  ./styling  
+   ./desktop  
+   ./hardware  
+   ./services	
+   ./styling  
    ./system  
-  ./tools
+   ./tools
   ];
 }
--- modules/styling/default.nix
+++ modules/styling/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
   ./qt
-  ./gkt
+  # ./gkt
   ./stylix
   ];
 }
