56b9fdd307ed7865fb747d22926b9f3da562db43
joygnu <mail@joygnu.org>
2024-08-31 17:47:11 +0200

    font

---
+++ modules/hyprland/style/font/default.nix
@@ -0,0 +1,6 @@
+{pkgs, ...}: {
+  fonts.packages = with pkgs; [
+    font-awesome
+    (nerdfonts.override {fonts = ["FiraCode"];})
+  ];
+}
--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,25 +1,12 @@
-{pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    font-awesome
-    (nerdfonts.override {fonts = ["FiraCode"];})
-  ];
+{...}: {
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  programs.nix-ld.enable = true;
-  services.printing.enable = true;
   time.timeZone = "Europe/Zurich";
   system.stateVersion = "23.11";
-  home-manager.backupFileExtension = "backup";
-  home-manager.users.joy.home = {
-    username = "joy";
-    homeDirectory = "/home/joy";
-    stateVersion = "23.11";
-    sessionVariables = {
-    };
-  };
 
   services.devmon.enable = true;
   services.udisks2.enable = true;
+  services.printing.enable = true;
   services.gvfs.enable = true;
 
     imports = [
