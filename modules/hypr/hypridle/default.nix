6e91950e14a01d6543a09439cb7a22fd44073473
joy <mail@joygnu.org>
2024-09-01 19:10:52 +0200

    libconfig for hypridle

--- modules/hypr/hypridle/default.nix
+++ modules/hypr/hypridle/default.nix
@@ -1,19 +1,26 @@
-{...}: {
-  home-manager.users.joy.services.hypridle = {
-    enable = true;
-    settings = {
-      listener = [
-        {
-          timeout = 300;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-        {
-          timeout = 600;
-          on-timeout = "systemctl suspend";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-      ];
+{lib,config,...}: {
+ options = {
+    hypridle.enable = lib.mkEnableOption "Enables kitty";
+  };
+  
+  config = lib.mkIf config.hypridle.enable {
+    home-manager.users.joy.services.hypridle = {
+      enable = true;
+      settings = {
+        listener = [
+          {
+            timeout = 300;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 600;
+            on-timeout = "systemctl suspend";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
+      };
     };
   };
+  
 }
