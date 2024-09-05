15f46a96e5d1d0c987a1179a7361072c2f56fe63
joygnu <mail@joygnu.org>
2024-09-05 18:49:51 +0200

    xdg

--- modules/system/homemanger/default.nix
+++ modules/system/homemanger/default.nix
@@ -1,12 +1,18 @@
 {...}: {
   home-manager.users.joy.programs.home-manager.enable = true;
-
-  home-manager.users.joy.xdg.mimeApps.defaultApplications = {
-    "text/plain" = ["hx.desktop"];
-    "image/*" = ["imv.desktop"];
-    "video/png" = ["mpv.desktop"];
-    "video/jpg" = ["mpv.desktop"];
-    "video/*" = ["mpv.desktop"];
+  home-manager.users.joy.xresources.path = ".config/.Xresources";
+  home-manager.users.joy.gtk.gtk2.configLocation = "/home/joy/.config/gtk-2.0/gtkrc";
+    home-manager.users.joy.xdg = {
+      mimeApps = {
+        enable = true;
+        defaultApplications = {
+        "text/plain" = ["hx.desktop"];
+        "image/*" = ["imv.desktop"];
+        "video/png" = ["mpv.desktop"];
+        "video/jpg" = ["mpv.desktop"];
+        "video/*" = ["mpv.desktop"];
+      };
+    };
   };
 
   home-manager.backupFileExtension = "backup";
