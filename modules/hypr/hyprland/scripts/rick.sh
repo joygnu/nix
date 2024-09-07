fb73e88eb0e04ae6cdcdde3bf9c3995311af6912
joy <mail@joygnu.org>
2024-09-07 14:40:19 +0200

    clown

---
+++ modules/hypr/hyprland/scripts/rick.sh
@@ -0,0 +1,3 @@
+mpv ~/nix/modules/hypr/hyprland/scripts/lol.webm & 
+sleep 0.5
+hyprctl dispatch fullscreen
--- modules/system/misc/default.nix
+++ modules/system/misc/default.nix
@@ -2,4 +2,5 @@
   nix.settings.experimental-features = ["nix-command" "flakes"];
   time.timeZone = "Europe/Zurich";
   system.stateVersion = "23.11";
+  services.logind.powerKey = "ignore";
 }
