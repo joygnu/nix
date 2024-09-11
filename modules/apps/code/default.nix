f926197adc4975902453faa69b0cbe4775403b08
joygnu <mail@joygnu.org>
2024-09-12 00:15:54 +0200

    some code changes

--- modules/apps/code/default.nix
+++ modules/apps/code/default.nix
@@ -3,9 +3,9 @@
   enable = true;
   package = pkgs.vscodium;
   extensions = with pkgs.vscode-extensions; [
-    dracula-theme.theme-dracula
-    vscodevim.vim
-    yzhang.markdown-all-in-one
+    ms-dotnettools.csharp
+    jdinhlife.gruvbox
+    asvetliakov.vscode-neovim
   ];
 };
 }
