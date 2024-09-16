40b4f3670fcc3b0b7d0bf4c38b62800ce1f167d3
joy <mail@joygnu.org>
2024-09-16 22:05:52 +0200

    dunst

--- modules/services/dunst/default.nix
+++ modules/services/dunst/default.nix
@@ -1,27 +1,35 @@
-{...}: {
+{ lib, ... }: {
   home-manager.users.joy.services.dunst = {
     enable = true;
-    # settings = {
-    # global = {
-    # frame_color = "#89b4fa";
-    # separator_color = "frame";
-    # };
+    settings = {
+      global = {
+        background = lib.mkForce "#282828";
+        foreground = lib.mkForce "#D5C4A1";
+        geometry = "300x60-3+200";
+      padding = 8;
+      horizontal_padding = 8;
+      separator_height = 2;
+      frame_width = 4;
+      corner_radius = 5;
+      font = lib.mkForce "DejaVuSansMono 12";
+      alignment = "center";
+      vertical_alignment = "center";
+      ellipsize = "middle";
+      ignore_newline = false;
+        
+      };
 
-    # urgency_low = {
-    # background = "#1e1e2e";
-    # foreground = "#cdd6f4";
-    # };
+      urgency_low = {
+        frame_color = lib.mkForce "#98971a";
+      };
 
-    # urgency_normal = {
-    # background = "#1e1e2e";
-    # foreground = "#cdd6f4";
-    # };
+      urgency_normal = {
+        frame_color = lib.mkForce "#458588";
+      };
 
-    # urgency_critical = {
-    # background = "#1e1e2e";
-    # foreground = "#cdd6f4";
-    # frame_color = "#fab387";
-    # };
-    # };
+      urgency_critical = {
+        frame_color = lib.mkForce "#98971a";
+      };
+    };
   };
 }
