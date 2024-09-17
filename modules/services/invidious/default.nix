f0958e928ce592b530573d12d747c313f9ff3e0a
joy <mail@joygnu.org>
2024-09-17 19:27:45 +0200

    invidious

---
+++ modules/services/invidious/default.nix
@@ -0,0 +1,12 @@
+{...}: {
+  services.invidious = {
+    enable = true;
+    settings = {
+      db = {
+        user = "invidious";
+        dbname = "invidious";
+      };
+      host_binding = "0.0.0.0";
+    };
+  };
+}
