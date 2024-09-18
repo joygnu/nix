6141819a26943c3ce489cef10d1431cfdada2f47
joygnu <mail@joygnu.org>
2024-09-18 19:27:14 +0200

    suyu

---
+++ modules/apps/gui/games/suyu/default.nix
@@ -0,0 +1,33 @@
+{pkgs, ...}: let
+  customPkgs =
+    pkgs
+    // {
+      suyu = pkgs.appimageTools.wrapType1 {
+        name = "suyu";
+        version = "0.0.3";
+
+        src = pkgs.fetchurl {
+          url = "https://git.suyu.dev/suyu/suyu/releases/download/v0.0.3/Suyu-Linux_x86_64.AppImage";
+          hash = "sha256-26sWhTvB6K1i/K3fmwYg5pDIUi+7xs3dz8yVj5q7H0c=";
+        };
+      };
+    };
+in {
+  environment.systemPackages = [
+    customPkgs.suyu
+  ];
+
+  home-manager.users.joy.xdg = {
+    desktopEntries = {
+      suyu = {
+        name = "Suyu";
+        exec = "suyu";
+        icon = "suyu";
+        terminal = false;
+        type = "Application";
+        categories = ["Game" "Emulator"];
+        mimeType = ["text/plain"];
+      };
+    };
+  };
+}
