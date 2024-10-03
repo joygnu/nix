9669edbca1b31bea3ba1ab831d6e7071baff0327
joy <mail@joygnu.org>
2024-10-03 21:32:31 +0200

    revmoing binaris

--- modules/apps/gui/games/norisk/default.nix
+++ modules/apps/gui/games/norisk/default.nix
@@ -13,10 +13,11 @@
         src = pkgs.fetchurl {
           url = "https://github.com/NoRiskClient/noriskclient-launcher/releases/download/v0.5.10/NoRiskClient-Linux.AppImage";
           hash = "sha256-y5kXP3kEW0UsV04JBeJCvSOuJf3/zo5s7ZigOpxbwkk=";
-                    
         };
       };
     };
+  backgroundUrl = "https://raw.githubusercontent.com/NoRiskClient/noriskclient-launcher/refs/heads/main/src/images/norisk_logo.png";
+  backgroundSha256 = "sha256-VwWwShUrT055mcabS8QTqqb8INgRB/08U2qEEIVYHlg=";
 in {
   environment.systemPackages = [
     customPkgs.noriskclient
@@ -27,7 +28,10 @@ in {
       NoRiskClient = {
         name = "NoRisk Client";
         exec = "noriskclient";
-        icon = lib.mkForce "/home/joy/nix/modules/apps/gui/games/norisk/norisk.png";
+        icon = pkgs.fetchurl {
+          url = backgroundUrl;
+          sha256 = backgroundSha256;
+        };
         terminal = false;
         type = "Application";
         categories = ["Game"];
