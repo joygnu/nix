cf603e5560066112ceae7c550513273dfcaad6d7
joy <mail@joygnu.org>
2024-10-08 16:04:51 +0200

    500

--- modules/wm/gnome/default.nix
+++ modules/wm/gnome/default.nix
@@ -1,52 +1,12 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: {
   options = {
     gnome.enable = lib.mkEnableOption "";
   };
-
   config = lib.mkIf config.gnome.enable {
     services.xserver.desktopManager.gnome.enable = true;
-
-    # environment.gnome.excludePackages = with pkgs; [
-    # gnome-backgrounds
-    # gnome-bluetooth
-    # gnome-color-manager
-    # gnome-control-center
-    # gnome-shell-extensions
-    # gnome-themes-extra
-    # gnome-tour
-    # gnome-user-docs
-    # orca
-    # glib
-    # gnome-menus
-    # gtk3.out
-    # xdg-user-dirs
-    # baobab
-    # epiphany
-    # gnome-text-editor
-    # gnome-calculator
-    # gnome-calendar
-    # gnome-characters
-    # gnome-clocks
-    # gnome-console
-    # gnome-contacts
-    # gnome-font-viewer
-    # gnome-logs
-    # gnome-maps
-    # gnome-music
-    # gnome-system-monitor
-    # gnome-weather
-    # loupe
-    # nautilus
-    # gnome-connections
-    # simple-scan
-    # snapshot
-    # totem
-    # yelp
-    # ];
   };
 }
