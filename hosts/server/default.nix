6c4a417be2449f52b9d8ec3de0043466c2ea59bc
joygnu <mail@joygnu.org>
2024-10-14 16:34:45 +0200

    fix server not booting

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -3,7 +3,7 @@
   services.logind.lidSwitch = "ignore";
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+
   imports = [
     ./hardware.nix
     ../../modules/programs
