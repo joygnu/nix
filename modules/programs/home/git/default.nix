969df84201c60f8d6c1f8934a5b5c3a9c2b65a6a
joygnu <mail@joygnu.org>
2024-10-16 14:19:54 +0200

    fix maby

--- modules/programs/home/git/default.nix
+++ modules/programs/home/git/default.nix
@@ -5,7 +5,7 @@
     userName = "joygnu";
     extraConfig = {
       init.defaultBranch = "main";
-      safe.directory = "~/nix/.git";
+      safe.directory = "/home/joy/nix/.git";
     };
   };
 }
