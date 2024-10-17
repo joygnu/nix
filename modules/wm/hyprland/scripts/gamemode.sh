bb89ed3115c5749ed27127c24019198b8d954e33
joygnu <mail@joygnu.org>
2024-10-17 21:19:16 +0200

    this shoud work ig fixed doas paus man sit

--- modules/wm/hyprland/scripts/gamemode.sh
+++ modules/wm/hyprland/scripts/gamemode.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!bin/sh
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
     hyprctl --batch "\
--- modules/wm/hyprland/scripts/mpv.sh
+++ modules/wm/hyprland/scripts/mpv.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-# Get the URL from the clipboard
 link=$(wl-paste)
 
-# Check if the link is not empty
 if [ -n "$link" ]; then
-    # Notify the user that the link is about to be opened
     notify-send "Opening Link" "$link"
 
-    # Try to open the link with mpv and capture any errors
     error_message=$(mpv "$link" 2>&1)
 
     if [ $? -ne 0 ]; then
-        # If mpv fails, notify the user with the error message
         notify-send "Error" "Failed to open the link with mpv:\n$error_message"
     fi
 else
