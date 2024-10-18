3951cee6db2f8d8cc1f471bf1db43ff3b97a2b04
joy <mail@joygnu.org>
2024-10-18 10:48:54 +0200

    improved rebuild script

--- modules/programs/home/zsh/scripts/rebuild.sh
+++ modules/programs/home/zsh/scripts/rebuild.sh
@@ -2,22 +2,30 @@
 
 current_dir=$(pwd)
 
-cd ~/nix
+cd ~/nix || exit
 
-git add .
+git fetch
 
-alejandra .
+if git diff --quiet HEAD..origin/main; then
+    echo "No changes detected in the remote. Proceeding with rebuild..."
 
-hostname=$(hostname)
+    git add .
 
-if echo "$hostname" | grep -q 'desktop'; then
-    doas nixos-rebuild switch --flake ~/nix/#desktop 
-elif echo "$hostname" | grep -q 'laptop'; then
-    doas nixos-rebuild switch --flake ~/nix/#laptop 
-elif echo "$hostname" | grep -q 'server'; then
-    doas nixos-rebuild switch --flake ~/nix/#server
-elif echo "$hostname" | grep -q 'localhost'; then
-    nix-on-droid switch --flake ~/nix
+    hostname=$(hostname)
+
+    if echo "$hostname" | grep -q 'desktop'; then
+        doas nixos-rebuild switch --flake ~/nix/#desktop
+    elif echo "$hostname" | grep -q 'laptop'; then
+        doas nixos-rebuild switch --flake ~/nix/#laptop
+    elif echo "$hostname" | grep -q 'server'; then
+        doas nixos-rebuild switch --flake ~/nix/#server
+    elif echo "$hostname" | grep -q 'localhost'; then
+        nix-on-droid switch --flake ~/nix
+    fi
+else
+    echo "Changes detected in the remote. Stopping execution."
+    git diff HEAD..origin/main
+    exit 1
 fi
 
-cd "$current_dir"
+cd "$current_dir" || exit
