145570bdaac76751f725fce2e88ff114e293de5d
joygnu <mail@joygnu.org>
2024-10-19 20:15:02 +0200

    24 colors for foot

--- modules/programs/home/foot/default.nix
+++ modules/programs/home/foot/default.nix
@@ -1,20 +1,31 @@
-{lib, ...}: {
+{
   programs.foot = {
     enable = true;
     settings = {
       main = {
         pad = "2x2";
+        font = "RobotoMono Nerd Font:size=12";
       };
 
       colors = {
-        "bright0" = lib.mkForce "504945";
-        "bright1" = lib.mkForce "fb4934";
-        "bright2" = lib.mkForce "b8bb26";
-        "bright3" = lib.mkForce "fabd2f";
-        "bright4" = lib.mkForce "83a598";
-        "bright5" = lib.mkForce "d3869b";
-        "bright6" = lib.mkForce "8ec07c";
-        "bright7" = lib.mkForce "ebdbb2";
+        "foreground" = "d5c4a1";
+        "background" = "282828";
+        "regular0" = "282828";
+        "regular1" = "cc241d";
+        "regular2" = "98971a";
+        "regular3" = "d79921";
+        "regular4" = "458588";
+        "regular5" = "b16286";
+        "regular6" = "689d6a";
+        "regular7" = "d5c4a1";
+        "bright0" = "504945";
+        "bright1" = "fb4934";
+        "bright2" = "b8bb26";
+        "bright3" = "fabd2f";
+        "bright4" = "83a598";
+        "bright5" = "d3869b";
+        "bright6" = "8ec07c";
+        "bright7" = "ebdbb2";
       };
     };
   };
