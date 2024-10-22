b7435aff76a4fed162082d323542b36fc7025e83
joygnu <mail@joygnu.org>
2024-10-22 08:55:01 +0200

    eh

--- modules/services/invidious/default.nix
+++ modules/services/invidious/default.nix
@@ -10,6 +10,6 @@
   };
   services.invidious.domain = "yt.joygnu.org";
   services.invidious.nginx.enable = true;
-  security.acme.defaults.email = "mail@joygnu.org";
+  security.acme.defaults.email = "contact@joygnu.org";
   security.acme.acceptTerms = true;
 }
