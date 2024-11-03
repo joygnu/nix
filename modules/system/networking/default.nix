d117048cdb9bf864237a7c998634990505f70f90
joy <mail@joygnu.org>
2024-11-03 12:35:47 +0100

    dns

--- modules/system/networking/default.nix
+++ modules/system/networking/default.nix
@@ -1,5 +1,5 @@
 {
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
-  networking.nameservers = ["9.9.9.9"];
+  networking.nameservers = ["9.9.9.9" "149.112.112.112" "2620:fe::fe" "2620:fe::9"];
 }
