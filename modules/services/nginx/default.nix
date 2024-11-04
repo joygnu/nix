a7560789433ea5232c7ae3475fd5be392dd5b648
joy <mail@joygnu.org>
2024-11-04 13:00:43 +0100

    new domain for my website lol

--- modules/services/nginx/default.nix
+++ modules/services/nginx/default.nix
@@ -1,5 +1,15 @@
 {
   services.nginx = {
+    virtualHosts."xn--xck.xyz" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/www/www.joygnu.org";
+    };
+    virtualHosts."joygnu.org" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/www/www.joygnu.org";
+    };
     virtualHosts."www.joygnu.org" = {
       forceSSL = true;
       enableACME = true;
