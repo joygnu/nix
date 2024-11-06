073b5bfdef22b98d8f15eef4a1f829166851d3ae
joy <mail@joygnu.org>
2024-11-06 19:04:28 +0100

    wallpaper

--- modules/services/nginx/default.nix
+++ modules/services/nginx/default.nix
@@ -30,6 +30,11 @@
       enableACME = true;
       locations."/".proxyPass = "http://localhost:5000";
     };
+    virtualHosts."wallpapers.joygnu.org" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/www/wallpapers.joygnu.org";
+    };
   };
   security.acme.certs = {
     "www.joygnu.org".email = "contact@joygnu.org";
