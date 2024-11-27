3630f4585da4b2a08b287f40b27de3d854cc8266
joy <mail@joygnu.org>
2024-11-27 11:39:20 +0100

    remove bad fastfetch config

--- modules/programs/fastfetch/default.nix
+++ modules/programs/fastfetch/default.nix
@@ -1,29 +1,5 @@
 {
   programs.fastfetch = {
     enable = true;
-    settings = {
-      logo = {
-        source = "nixos_small";
-        padding = {
-          right = 3;
-        };
-      };
-      display = {
-        color = "blue";
-      };
-      modules = [
-        "title"
-        "os"
-        "kernel"
-        "uptime"
-        "packages"
-        "shell"
-        "cpu"
-        "memory"
-        "disk"
-        "break"
-        "colors"
-      ];
-    };
   };
 }
