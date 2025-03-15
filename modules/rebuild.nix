672fc6f5fc91dc16b26a3b2e08a508564d74363e
joy <contact@joygnu.org>
2025-03-15 16:22:38 +0100

    improve rebuild script

--- modules/rebuild.nix
+++ modules/rebuild.nix
@@ -14,20 +14,11 @@
 
     current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-    if git diff --quiet HEAD..origin/"$current_branch"; then
-      echo "No changes detected in the remote. Proceeding with rebuild..."
-    else
-      echo "Changes detected in the remote."
-      git diff HEAD..origin/"$current_branch"
-
+    if ! git diff --quiet HEAD..origin/"$current_branch"; then
       read -p "Do you want to pull the changes? (y/n): " pull_changes
 
       if [[ "$pull_changes" == "y" || "$pull_changes" == "Y" ]]; then
-        echo "Pulling changes from remote..."
         git pull origin "$current_branch"
-      else
-        echo "Aborting rebuild."
-        exit 1
       fi
     fi
 
