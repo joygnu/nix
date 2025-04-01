ae2afa2622198f0ae7dd26dd836d8af20b1f2568
joy <contact@joygnu.org>
2025-04-01 21:12:09 +0200

    minecraft server chunky

--- modules/services/minecraft-server/default.nix
+++ modules/services/minecraft-server/default.nix
@@ -26,7 +26,11 @@
           mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
             Fabric-API = pkgs.fetchurl {
               url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/UnrycCWP/fabric-api-0.115.1%2B1.21.4.jar";
-              sha512 = "3m2x4wrl7gmdjw1739a2d5kk4nxpdndhsk2v87vw7w0fl040sgsjpvc5li2kqdm3h58k0fk7vsrq6l6z20w87v5drwckvdmg5vgisfm";
+              sha512 = "1en4dnm17cl4bmUfxIH4hhqc9T7TgYkKHLXhKSItbF+pnwYEUAf44fugLaaGzbbbLZmzNKHSOIHLVt+hmZMu6g==";
+            };
+            Chunky = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/fALzjamp/versions/VkAgASL1/Chunky-Fabric-1.4.27.jar";
+              sha512 = "qJ+UlH58OZLgHka+iWfSplkzNDM6VGtP/1/bAqH1prg8k63Exyqbmx8U+Sme/KqKXX9e7t89pUHH5yq8Xickxg==";
             };
           });
         };
