8fddd58abe2e2ee34aedf445f94c57139c92de89
joy <contact@joygnu.org>
2025-04-22 14:01:16 +0200

    fix dns

--- modules/hardware/networking/default.nix
+++ modules/hardware/networking/default.nix
@@ -1,4 +1,11 @@
 {
   networking.networkmanager.enable = true;
-  networking.nameservers = ["9.9.9.9" "149.112.112.112" "2620:fe::fe" "2620:fe::9"];
+  networking.nameservers = ["9.9.9.9"];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    fallbackDns = ["149.112.112.112"];
+    dnsovertls = "true";
+  };
 }
