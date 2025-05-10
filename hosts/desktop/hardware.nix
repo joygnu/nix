a2aaa1acd96ada52c36fe8e377f1709c28c5225a
joy <contact@joygnu.org>
2025-05-10 13:05:20 +0200

    wakeonlan desktop

--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -27,6 +27,7 @@
   };
 
   networking.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp6s0.wakeOnLan.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
