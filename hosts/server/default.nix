e34fad1d88a0e3cb9787b65f66558dff0bd8deee
joy <contact@joygnu.org>
2025-06-12 21:52:40 +0200

    switch vpn

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -6,15 +6,5 @@
 
   services.logind.lidSwitch = "ignore";
 
-  forgejo.enable = true;
-  immich.enable = true;
-  nginx.enable = true;
-  radicale.enable = true;
-  syncthing-server.enable = true;
-  redlib.enable = true;
-  home-assistant.enable = true;
-  searx.enable = true;
-  invidious.enable = true;
-
   imports = [./hardware.nix];
 }
--- hosts/vpn/default.nix
+++ hosts/vpn/default.nix
@@ -6,6 +6,15 @@
 
   vpn-home.enable = true;
   minecraft-server.enable = true;
+  forgejo.enable = true;
+  immich.enable = true;
+  nginx.enable = true;
+  radicale.enable = true;
+  syncthing-server.enable = true;
+  redlib.enable = true;
+  home-assistant.enable = true;
+  searx.enable = true;
+  invidious.enable = true;
 
   imports = [./hardware.nix];
 }
