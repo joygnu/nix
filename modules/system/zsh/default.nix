2272d058a55ce5e93723c8c892346b1099b2f710
joy <contact@joygnu.org>
2025-08-06 23:15:41 +0200

    update

--- modules/system/zsh/default.nix
+++ modules/system/zsh/default.nix
@@ -4,43 +4,40 @@
   username,
   nixpath,
   ...
-}: let
-  config = {
-    programs.zsh = {
-      enable = true;
-      syntaxHighlighting.enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      dotDir = ".config/zsh";
-      history.size = 1000000;
-      history.path = ".config/zsh/history";
-
-      shellAliases = {
-        ls = "${pkgs.eza}/bin/eza --icons -a --group-directories-first";
-        sp = "${pkgs.ncdu}/bin/ncdu";
-        v = "${pkgs.helix}/bin/hx";
-        cat = "${pkgs.bat}/bin/bat";
-        lg = "${pkgs.lazygit}/bin/lazygit";
-        wp = "${pkgs.wakeonlan}/bin/wakeonlan 18:c0:4d:ee:c1:21";
-        ms = "z ~ && ssh ${username}@mail.${domain.a}";
-        hs = "z ~ && ssh ${username}@${domain.a}";
-        fr = "doas systemctl reboot --firmware";
-        up = "nix flake update --flake ${nixpath}";
-        del = "doas nix-collect-garbage -d";
-      };
-
-      oh-my-zsh = {
-        enable = true;
-        plugins = ["git" "history-substring-search"];
-        theme = "simple";
-      };
-    };
-  };
-in {
+}: {
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   home-manager.users = {
-    ${username} = config;
-    root = config;
+    ${username} = {
+      programs.zsh = {
+        enable = true;
+        syntaxHighlighting.enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        dotDir = "/home/joy/.config/zsh";
+        history.size = 1000000;
+        history.path = "/home/joy/.config/zsh/history";
+
+        shellAliases = {
+          ls = "${pkgs.eza}/bin/eza --icons -a --group-directories-first";
+          sp = "${pkgs.ncdu}/bin/ncdu";
+          v = "${pkgs.helix}/bin/hx";
+          cat = "${pkgs.bat}/bin/bat";
+          lg = "${pkgs.lazygit}/bin/lazygit";
+          wp = "${pkgs.wakeonlan}/bin/wakeonlan 18:c0:4d:ee:c1:21";
+          ms = "z ~ && ssh ${username}@mail.${domain.a}";
+          hs = "z ~ && ssh ${username}@${domain.a}";
+          fr = "doas systemctl reboot --firmware";
+          up = "nix flake update --flake ${nixpath}";
+          del = "doas nix-collect-garbage -d";
+        };
+
+        oh-my-zsh = {
+          enable = true;
+          plugins = ["git" "history-substring-search"];
+          theme = "simple";
+        };
+      };
+    };
   };
 }
