a4c5097604c6ee6f880dc90fe3827a8edada6cb2
joy <contact@joygnu.org>
2026-02-11 15:45:51 +0100

    minecraft speedrun

--- modules/gaming/minecraft/default.nix
+++ modules/gaming/minecraft/default.nix
@@ -1,15 +1,88 @@
 {
-  lib,
   config,
+  lib,
   pkgs,
+  username,
   ...
-}: {
-  options = {
-    minecraft.enable = lib.mkEnableOption "";
+}: let
+  inherit (lib) mkEnableOption mkIf getExe;
+
+  cfg = config.minecraft;
+
+  ninjabrain-bot = pkgs.fetchurl {
+    url = "https://github.com/Ninjabrain1/Ninjabrain-Bot/releases/download/1.5.1/Ninjabrain-Bot-1.5.1.jar";
+    sha256 = "sha256-Rxu9A2EiTr69fLBUImRv+RLC2LmosawIDyDPIaRcrdw=";
   };
-  config = lib.mkIf config.minecraft.enable {
-    environment.systemPackages = [
-      pkgs.prismlauncher
-    ];
+in {
+  options.minecraft = {
+    enable = mkEnableOption "Minecraft with PrismLauncher + Waywall";
+  };
+
+  config = mkIf cfg.enable {
+    home-manager.users.${username} = {
+      home.packages = with pkgs; [
+        (prismlauncher.override {
+          jdks = [
+            temurin-jre-bin-8
+            temurin-jre-bin-17
+            temurin-jre-bin-21
+          ];
+          additionalLibs = [
+            libXt
+            libXtst
+            libxkbcommon
+          ];
+        })
+
+        waywall
+      ];
+
+      home.file.".config/waywall/init.lua".text =
+        # lua
+        ''
+          local waywall = require("waywall")
+          local helpers = require("waywall.helpers")
+
+          local resolution_toggle = true
+
+          local config = {
+            theme = {
+              background = "#282828ff",
+              ninb_anchor = "bottomleft",
+            },
+            input = {
+              layout = "us",
+              remaps = {
+                ["tab"] = "F3",
+                ["capslock"] = "esc",
+              },
+              repeat_rate = 40,
+              repeat_delay = 300,
+              confine_pointer = false,
+            },
+          }
+
+          config.actions = {
+            ["Shift-G"] = function()
+              if resolution_toggle then
+                waywall.set_resolution(2560, 1440)
+              else
+                waywall.set_resolution(800, 1440)
+              end
+              resolution_toggle = not resolution_toggle
+            end,
+
+            ["Ctrl-Shift-N"] = function()
+              waywall.exec("${getExe pkgs.temurin-jre-bin-17} -jar ${ninjabrain-bot} -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel")
+            end,
+
+            ["F7"] = function()
+              helpers.toggle_floating()
+            end,
+          }
+
+          return config
+        '';
+    };
   };
 }
--- modules/programs/firefox/userChrome.nix
+++ modules/programs/firefox/userChrome.nix
@@ -8,238 +8,240 @@
     home-manager.users.${username} = {
       programs.firefox = {
         profiles.${username} = {
-          userChrome = ''
-            :root {
-              --background: #${config.home-manager.users.${username}.stylix.base16Scheme.base01};
-              --secondary: #${config.home-manager.users.${username}.stylix.base16Scheme.base00};
-              --foreground: #${config.home-manager.users.${username}.stylix.base16Scheme.base05};
-              --orange-highlight: #${config.home-manager.users.${username}.stylix.base16Scheme.base09};
-              --sound-border: #${config.home-manager.users.${username}.stylix.base16Scheme.base0E};
-              --separator: #${config.home-manager.users.${username}.stylix.base16Scheme.base03};
-              --tab-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base02};
-              --tab-btn-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base02};
-              --toolbar-btn-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base03};
-              --url-bar: #${config.home-manager.users.${username}.stylix.base16Scheme.base01};
-              --url-focus: #${config.home-manager.users.${username}.stylix.base16Scheme.base01};
-              --url-bar-item-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base03};
-              --sidebar: #${config.home-manager.users.${username}.stylix.base16Scheme.base01};
-              --sidebar-button-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base04};
-              --sidebar-highlight: #${config.home-manager.users.${username}.stylix.base16Scheme.base0D};
-              --toolbar-bgcolor: var(--secondary) !important;
-              --tab: var(--background);
-              --tab-btn: var(--tab-inactive);
-              --tab-inactive: var(--secondary);
-              --tab-btn-inactive: var(--tab-inactive);
-            }
+          userChrome =
+            #css
+            ''
+              :root {
+                --background: #${config.home-manager.users.${username}.stylix.base16Scheme.base01};
+                --secondary: #${config.home-manager.users.${username}.stylix.base16Scheme.base00};
+                --foreground: #${config.home-manager.users.${username}.stylix.base16Scheme.base05};
+                --orange-highlight: #${config.home-manager.users.${username}.stylix.base16Scheme.base09};
+                --sound-border: #${config.home-manager.users.${username}.stylix.base16Scheme.base0E};
+                --separator: #${config.home-manager.users.${username}.stylix.base16Scheme.base03};
+                --tab-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base02};
+                --tab-btn-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base02};
+                --toolbar-btn-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base03};
+                --url-bar: #${config.home-manager.users.${username}.stylix.base16Scheme.base01};
+                --url-focus: #${config.home-manager.users.${username}.stylix.base16Scheme.base01};
+                --url-bar-item-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base03};
+                --sidebar: #${config.home-manager.users.${username}.stylix.base16Scheme.base01};
+                --sidebar-button-hover: #${config.home-manager.users.${username}.stylix.base16Scheme.base04};
+                --sidebar-highlight: #${config.home-manager.users.${username}.stylix.base16Scheme.base0D};
+                --toolbar-bgcolor: var(--secondary) !important;
+                --tab: var(--background);
+                --tab-btn: var(--tab-inactive);
+                --tab-inactive: var(--secondary);
+                --tab-btn-inactive: var(--tab-inactive);
+              }
 
-            .titlebar-buttonbox-container {
-              display: none !important;
-            }
+              .titlebar-buttonbox-container {
+                display: none !important;
+              }
 
-            hbox.titlebar-spacer {
-              display: none !important;
-            }
+              hbox.titlebar-spacer {
+                display: none !important;
+              }
 
-            .panel-arrowcontainer {
-                background-color: var(--secondary) !important;
-            }
+              .panel-arrowcontainer {
+                  background-color: var(--secondary) !important;
+              }
 
-            #PopupAutoComplete, #PopupSearchAutoComplete, panelview {
-                background-color: var(--url-focus) !important;
-                color: var(--foreground) !important;
-            }
+              #PopupAutoComplete, #PopupSearchAutoComplete, panelview {
+                  background-color: var(--url-focus) !important;
+                  color: var(--foreground) !important;
+              }
 
-            panel[type="autocomplete-richlistbox"] {
-                --panel-background: none !important;
-                border-radius: 4px !important;
-                color: var(--foreground) !important;
-            }
+              panel[type="autocomplete-richlistbox"] {
+                  --panel-background: none !important;
+                  border-radius: 4px !important;
+                  color: var(--foreground) !important;
+              }
 
-            toolbarseparator, menuseparator {
-                border-color: var(--separator) !important;
-                border-image: none !important;
-            }
+              toolbarseparator, menuseparator {
+                  border-color: var(--separator) !important;
+                  border-image: none !important;
+              }
 
-            toolbar {
-                background-color: var(--secondary);
-            }
+              toolbar {
+                  background-color: var(--secondary);
+              }
 
-            toolbar#TabsToolbar {
-                background-color: var(--background);
-            }
+              toolbar#TabsToolbar {
+                  background-color: var(--background);
+              }
 
-            toolbar#TabsToolbar:-moz-window-inactive {
-                background-color: var(--toolbar-bgcolor) !important;
-            }
+              toolbar#TabsToolbar:-moz-window-inactive {
+                  background-color: var(--toolbar-bgcolor) !important;
+              }
 
-            #titlebar, #titlebar-spacer, #titlebar-buttonbox-container {
-                background-color: var(--background) !important;
-                border: none !important;
-            }
+              #titlebar, #titlebar-spacer, #titlebar-buttonbox-container {
+                  background-color: var(--background) !important;
+                  border: none !important;
+              }
 
-            #navigator-toolbox {
-                --tabs-border-color: var(--orange-highlight) !important;
-                border: none !important;
-            }
+              #navigator-toolbox {
+                  --tabs-border-color: var(--orange-highlight) !important;
+                  border: none !important;
+              }
 
-            #navigator-toolbox::after {
-                border-bottom: 0px !important;
-            }
+              #navigator-toolbox::after {
+                  border-bottom: 0px !important;
+              }
 
-            #urlbar, #searchbar {
-                box-shadow: none !important;
-                border: none !important;
-                border-radius: 4px;
-                color: var(--foreground) !important;
-                background-color: var(--url-bar) !important;
-                --autocomplete-popup-separator-color: var(--separator) !important;
-            }
+              #urlbar, #searchbar {
+                  box-shadow: none !important;
+                  border: none !important;
+                  border-radius: 4px;
+                  color: var(--foreground) !important;
+                  background-color: var(--url-bar) !important;
+                  --autocomplete-popup-separator-color: var(--separator) !important;
+              }
 
-            #urlbar-input, #urlbar-input-container {
-                color: var(--foreground) !important;
-                background-color: var(--url-bar) !important;
-            }
+              #urlbar-input, #urlbar-input-container {
+                  color: var(--foreground) !important;
+                  background-color: var(--url-bar) !important;
+              }
 
-            #urlbar:not([open]) #urlbar-input-container:focus-within, #searchbar:focus-within {
-                border: 2px solid var(--orange-highlight) !important;
-            }
+              #urlbar:not([open]) #urlbar-input-container:focus-within, #searchbar:focus-within {
+                  border: 2px solid var(--orange-highlight) !important;
+              }
 
-            #urlbar:focus-within, #urlbar[open], #urlbar[open] #urlbar-input-container, #urlbar[open] #urlbar-input, #urlbar-input-container:focus-within, #urlbar-input:focus, #urlbar-background, .urlbarView, #searchbar:focus-within, menupop {
-                background-color: var(--url-focus) !important;
-            }
+              #urlbar:focus-within, #urlbar[open], #urlbar[open] #urlbar-input-container, #urlbar[open] #urlbar-input, #urlbar-input-container:focus-within, #urlbar-input:focus, #urlbar-background, .urlbarView, #searchbar:focus-within, menupop {
+                  background-color: var(--url-focus) !important;
+              }
 
-            #searchbar:focus-within {
-                outline: none !important;
-            }
+              #searchbar:focus-within {
+                  outline: none !important;
+              }
 
-            :is(panel, menupopup)::part(content) {
-                background: none !important;
-            }
+              :is(panel, menupopup)::part(content) {
+                  background: none !important;
+              }
 
-            .urlbarView button:hover, #searchbar button:hover, .urlbarView-row:hover .urlbarView-row-inner, .search-autocomplete-richlistbox-popup .autocomplete-richlistitem:hover {
-                background-color: var(--url-bar-item-hover) !important;
-            }
+              .urlbarView button:hover, #searchbar button:hover, .urlbarView-row:hover .urlbarView-row-inner, .search-autocomplete-richlistbox-popup .autocomplete-richlistitem:hover {
+                  background-color: var(--url-bar-item-hover) !important;
+              }
 
-            .urlbarView-row[type="switchtab"] > span {
-                color: var(--orange-highlight) !important;
-            }
+              .urlbarView-row[type="switchtab"] > span {
+                  color: var(--orange-highlight) !important;
+              }
 
-            #PopupSearchAutoComplete .autocomplete-richlistitem[selected], .searchbar-engine-one-off-item[selected], .urlbarView-row[selected], .urlbarView-row[aria-selected="true"], .urlbarView-row:not([type="tip"], [type="dynamic"])[selected] > .urlbarView-row-inner, .urlbarView-row-inner[selected] {
-                color: var(--foreground) !important;
-                background-color: var(--sidebar-highlight) !important;
-                border-radius: 4px !important;
-            }
+              #PopupSearchAutoComplete .autocomplete-richlistitem[selected], .searchbar-engine-one-off-item[selected], .urlbarView-row[selected], .urlbarView-row[aria-selected="true"], .urlbarView-row:not([type="tip"], [type="dynamic"])[selected] > .urlbarView-row-inner, .urlbarView-row-inner[selected] {
+                  color: var(--foreground) !important;
+                  background-color: var(--sidebar-highlight) !important;
+                  border-radius: 4px !important;
+              }
 
-            #PersonalToolbar {
-                background-color: var(--secondary) !important;
-            }
+              #PersonalToolbar {
+                  background-color: var(--secondary) !important;
+              }
 
-            .tab-background[selected="true"] {
-                background-color: transparent !important;
-                background-image: none !important;
-            }
+              .tab-background[selected="true"] {
+                  background-color: transparent !important;
+                  background-image: none !important;
+              }
 
-            .tabbrowser-tab:hover > .tab-stack > .tab-background:not([selected="true"]):not([multiselected]) {
-                background-color: transparent !important;
-            }
+              .tabbrowser-tab:hover > .tab-stack > .tab-background:not([selected="true"]):not([multiselected]) {
+                  background-color: transparent !important;
+              }
 
-            .tabbrowser-tab[soundplaying="true"] {
-                --lwt-tab-line-color: var(--sound-border) !important;
-            }
+              .tabbrowser-tab[soundplaying="true"] {
+                  --lwt-tab-line-color: var(--sound-border) !important;
+              }
 
-            .tab-content[selected="true"] {
-                border-color: var(--orange-highlight) !important;
-                --sound-border: var(--orange-highlight) !important;
-            }
+              .tab-content[selected="true"] {
+                  border-color: var(--orange-highlight) !important;
+                  --sound-border: var(--orange-highlight) !important;
+              }
 
-            .tab-text {
-                color: var(--foreground);
-            }
+              .tab-text {
+                  color: var(--foreground);
+              }
 
-            tab, #tabbrowser-tabs {
-                background-color: var(--background) !important;
-                color: var(--foreground) !important;
-            }
+              tab, #tabbrowser-tabs {
+                  background-color: var(--background) !important;
+                  color: var(--foreground) !important;
+              }
 
-            tab:-moz-window-inactive, #tabbrowser-tabs:-moz-window-inactive, #nav-bar-customization-target {
-                background-color: var(--secondary) !important;
-            }
+              tab:-moz-window-inactive, #tabbrowser-tabs:-moz-window-inactive, #nav-bar-customization-target {
+                  background-color: var(--secondary) !important;
+              }
 
-            .tabbrowser-tab[selected] {
-                opacity: 1;
-            }
+              .tabbrowser-tab[selected] {
+                  opacity: 1;
+              }
 
-            .tab-loading-burst {
-                background-color: inherit;
-            }
+              .tab-loading-burst {
+                  background-color: inherit;
+              }
 
-            .tab-content {
-                border-radius: 4px;
-                margin: 5px 0px 5px 0px !important;
-                border: 1px solid transparent !important;
-            }
+              .tab-content {
+                  border-radius: 4px;
+                  margin: 5px 0px 5px 0px !important;
+                  border: 1px solid transparent !important;
+              }
 
-            .tab-content[selected="true"] {
-                background-color: var(--secondary) !important;
-                color: var(--foreground) !important;
-                border: 1px solid var(--orange-highlight) !important;
-            }
+              .tab-content[selected="true"] {
+                  background-color: var(--secondary) !important;
+                  color: var(--foreground) !important;
+                  border: 1px solid var(--orange-highlight) !important;
+              }
 
-            toolbarbutton {
-                fill: var(--foreground) !important;
-                background-color: transparent !important;
-            }
+              toolbarbutton {
+                  fill: var(--foreground) !important;
+                  background-color: transparent !important;
+              }
 
-            #TabsToolbar toolbarbutton {
-                border-radius: 4px;
-                border: 1px solid transparent !important;
-            }
+              #TabsToolbar toolbarbutton {
+                  border-radius: 4px;
+                  border: 1px solid transparent !important;
+              }
 
-            #TabsToolbar #firefox-view-button > .toolbarbutton-icon {
-                background-image: none !important;
-                border-radius: 4px;
-            }
+              #TabsToolbar #firefox-view-button > .toolbarbutton-icon {
+                  background-image: none !important;
+                  border-radius: 4px;
+              }
 
-            #TabsToolbar #firefox-view-button[open] > .toolbarbutton-icon {
-                background-color: var(--secondary) !important;
-                color: var(--foreground) !important;
-                padding: 9px !important;
-                border: 1px solid var(--orange-highlight) !important;
-            }
+              #TabsToolbar #firefox-view-button[open] > .toolbarbutton-icon {
+                  background-color: var(--secondary) !important;
+                  color: var(--foreground) !important;
+                  padding: 9px !important;
+                  border: 1px solid var(--orange-highlight) !important;
+              }
 
-            #TabsToolbar #firefox-view-button:not([open]):hover > .toolbarbutton-icon {
-                background-color: var(--tab-hover) !important;
-            }
+              #TabsToolbar #firefox-view-button:not([open]):hover > .toolbarbutton-icon {
+                  background-color: var(--tab-hover) !important;
+              }
 
-            #nav-bar toolbarbutton > .toolbarbutton-icon, #nav-bar toolbarbutton > .toolbarbutton-badge-stack {
-                border-radius: 6px;
-            }
+              #nav-bar toolbarbutton > .toolbarbutton-icon, #nav-bar toolbarbutton > .toolbarbutton-badge-stack {
+                  border-radius: 6px;
+              }
 
-            #nav-bar toolbarbutton:hover > .toolbarbutton-icon, #nav-bar toolbarbutton:hover > .toolbarbutton-badge-stack {
-                background-color: var(--toolbar-btn-hover) !important;
-            }
+              #nav-bar toolbarbutton:hover > .toolbarbutton-icon, #nav-bar toolbarbutton:hover > .toolbarbutton-badge-stack {
+                  background-color: var(--toolbar-btn-hover) !important;
+              }
 
-            #back-button {
-                padding-right: 2px !important;
-                padding-left: 2px !important;
-                margin-left: 6px !important;
-            }
+              #back-button {
+                  padding-right: 2px !important;
+                  padding-left: 2px !important;
+                  margin-left: 6px !important;
+              }
 
-            #PanelUI-menu-button {
-                background-color: var(--secondary) !important;
-                padding-right: 2px !important;
-                margin-right: 6px !important;
-            }
+              #PanelUI-menu-button {
+                  background-color: var(--secondary) !important;
+                  padding-right: 2px !important;
+                  margin-right: 6px !important;
+              }
 
-            #PersonalToolbar toolbarbutton {
-                margin: 3px 0px 3px 3px !important;
-                border-radius: 4px;
-            }
+              #PersonalToolbar toolbarbutton {
+                  margin: 3px 0px 3px 3px !important;
+                  border-radius: 4px;
+              }
 
-            #PersonalToolbar toolbarbutton:hover {
-                background-color: var(--toolbar-btn-hover) !important;
-            }
-          '';
+              #PersonalToolbar toolbarbutton:hover {
+                  background-color: var(--toolbar-btn-hover) !important;
+              }
+            '';
         };
       };
     };
